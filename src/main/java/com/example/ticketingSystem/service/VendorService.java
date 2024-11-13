c7e3cbeb71fd11c5cd1d23943e383943f5f98e9e
Nithila Thawalampitiya <nithilanimraka@gmail.com>
2024-11-13 23:17:33 +0530

    fixed Autowired error (@Service) in vendorserv

--- src/main/java/com/example/ticketingSystem/service/VendorService.java
+++ src/main/java/com/example/ticketingSystem/service/VendorService.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import com.example.ticketingSystem.repository.VendorRepository;
 import com.example.ticketingSystem.entity.Vendor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Slf4j
+@Service
 public class VendorService {
 
     @Autowired
