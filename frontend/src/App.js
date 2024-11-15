21fcdbf30e43e8e680dffd5a55654a52d67b9ba3
Nithila Thawalampitiya <nithilanimraka@gmail.com>
2024-11-15 22:54:22 +0530

    dummy home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 // import './App.css';
 import Login from './pages/login'
 import Signup from './pages/signup'
+import Home from './pages/home'
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/signup' element={<Signup />}></Route>
+        <Route path='/' element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
 
