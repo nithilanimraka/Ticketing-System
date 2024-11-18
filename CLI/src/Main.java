e3349061bb8b70247864c15fb5818dfbf3587ea4
Nithila Thawalampitiya <nithilanimraka@gmail.com>
2024-11-18 23:42:24 +0530

    created CLI folder

---
+++ CLI/src/Main.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+import java.util.logging.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(Main.class.getName());
+        
+    }
+}
